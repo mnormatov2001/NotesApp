7616cd5980a9c8b7fbbe8d380c05c2125e2861a8
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-07-08 19:49:20 +0300

    Configuring DataBase

---
+++ Notes.Data/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.Data
+{
+    internal class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
