b87dcf979dc784d074d032bf9928880fa0ed3887
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-07-09 05:16:22 +0300

    Modified Note.cs

--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -5,7 +5,7 @@
         public Guid UserId { get; set; }
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string Details { get; set; }
+        public string Content { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime EditDate { get; set; }
     }
