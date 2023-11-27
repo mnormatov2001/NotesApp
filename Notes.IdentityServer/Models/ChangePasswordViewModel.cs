9d51d9fce4f90e43ccae757d20c5347a20fd3f5f
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-11-27 19:02:36 +0300

    modified ChangePasswordViewModel

--- Notes.IdentityServer/Models/ChangePasswordViewModel.cs
+++ Notes.IdentityServer/Models/ChangePasswordViewModel.cs
@@ -9,6 +9,11 @@ namespace Notes.IdentityServer.Models
         [Required]
         [StringLength(20, MinimumLength = 5)]
         [RegularExpression(".*[a-z].*")]
+        [DataType(DataType.Password)]
         public string NewPassword { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(NewPassword))]
+        public string ConfirmPassword { get; set; }
     }
 }
